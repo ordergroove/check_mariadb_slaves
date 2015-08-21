b0195e7d7c8adcf2e681f703199c28926e6f65ee
Juan Gutierrez <juanny.gee@gmail.com>
2015-08-21 08:19:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='check-mariadb-slaves',
-    version='1.0',
+    version='1.1',
     include_package_data=True,
     packages=find_packages(),
     license="MIT",
     author="Juan Gutierrez",
+    author_email="juanny.gee@gmail.com",
     description="Nagios Plugin written in Python to monitor MariaDB slave metrics",
     url="https://github.com/ordergroove/check_mariadb_slaves"
 )
