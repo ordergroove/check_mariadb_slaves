15a6e90bf5300e2e7a23340c93d9e7d4ff8fbcbf
Juan Gutierrez <juanny.gee@gmail.com>
2018-06-10 11:05:26 -0400

    Add password sanitizing support for CREATE/ALTER USER queries

--- nagios_plugin/password_sanitizer.py
+++ nagios_plugin/password_sanitizer.py
@@ -10,8 +10,21 @@ def sanitize_set_password_syntax(msg):
     Captures variations of the SET PASSWORD syntax. See:https://mariadb.com/kb/en/library/set-password/
     """
     if "SET PASSWORD" in msg.upper():
-        return re.sub(r"=.*('|\")(\)?)", '= ***', msg)
+        msg = re.sub(r"=.*('|\")(\)?)", '= ***', msg, flags=re.IGNORECASE)
+    return msg
+
+def sanitize_user_identified_syntax(msg):
+    """
+    Captures variations of the ALTER/CREATE USER...IDENTIFIED... syntax.
+    See: https://mariadb.com/kb/en/library/alter-user/
+    """
+    if "ALTER USER" in msg.upper() or "CREATE USER" in msg.upper():
+        msg = re.sub(r"BY .*('|\")", 'BY ***', msg, flags=re.IGNORECASE)
+        msg = re.sub(r"USING .*('|\")", 'USING ***', msg, flags=re.IGNORECASE)
+        msg = re.sub(r"AS .*('|\")", 'AS ***', msg, flags=re.IGNORECASE)
     return msg
 
 def sanitize_passwords(msg):
-    return sanitize_set_password_syntax(msg)
+    for sanitize_fn in [sanitize_user_identified_syntax, sanitize_set_password_syntax]:
+        msg = sanitize_fn(msg)
+    return msg
