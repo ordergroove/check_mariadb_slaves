63cc8888bc09c129958773c6488c7b6f9119d855
Fede Alvarez <federico.alvarez@ordergroove.com>
2018-11-08 15:21:39 -0300

    Refactor LagFormatter into a method

--- nagios_plugin/lag_formatter.py
+++ nagios_plugin/lag_formatter.py
@@ -3,29 +3,25 @@
 import time
 from collections import OrderedDict
 
+PARTS_MAP = OrderedDict([
+    ('hour', 'hour'),
+    ('min', 'minute'),
+    ('sec', 'second')
+])
 
-class LagFormatter(object):
 
-    PARTS_MAP = OrderedDict([
-        ('hour', 'hour'),
-        ('min', 'minute'),
-        ('sec', 'second')
-    ])
+def format_lag(seconds):
+    seconds = time.gmtime(int(seconds))
+    parts = []
 
-    def format(self, seconds):
-        seconds = time.gmtime(int(seconds))
-        parts = []
+    for key in PARTS_MAP.keys():
+        value = getattr(seconds, 'tm_{}'.format(key), 0)
+        if value:
+            formatted_part = '{} {}{}'.format(
+                value,
+                PARTS_MAP.get(key),
+                's' if value > 1 else ''
+            )
+            parts.append(formatted_part)
 
-        for key in self.PARTS_MAP.keys():
-            value = getattr(seconds, 'tm_{}'.format(key), 0)
-            if value:
-                parts.append(self._get_formatted_part(key, value))
-
-        return ' '.join(parts)
-
-    def _get_formatted_part(self, key, value):
-        return '{} {}{}'.format(
-            value,
-            self.PARTS_MAP.get(key),
-            's' if value > 1 else ''
-        )
+    return ' '.join(parts)
--- tests/nagios_plugin/test_lag_formatter.py
+++ tests/nagios_plugin/test_lag_formatter.py
@@ -2,21 +2,18 @@
 
 import unittest
 
-from nagios_plugin.lag_formatter import LagFormatter
+from nagios_plugin.lag_formatter import format_lag
 
 
 class TestLagFormatter(unittest.TestCase):
 
-    def setUp(self):
-        self.lag_formatter = LagFormatter()
-
     def test_response_contains_all_parts(self):
-        self.assertEquals(self.lag_formatter.format(3661), '1 hour 1 minute 1 second')
+        self.assertEquals(format_lag(3661), '1 hour 1 minute 1 second')
 
     def test_response_ignores_empty_parts(self):
-        self.assertEquals(self.lag_formatter.format(3601), '1 hour 1 second')
-        self.assertEquals(self.lag_formatter.format(61), '1 minute 1 second')
-        self.assertEquals(self.lag_formatter.format(3600), '1 hour')
+        self.assertEquals(format_lag(3601), '1 hour 1 second')
+        self.assertEquals(format_lag(61), '1 minute 1 second')
+        self.assertEquals(format_lag(3600), '1 hour')
 
     def test_response_adds_plural_to_formatted_parts(self):
-        self.assertEquals(self.lag_formatter.format(7322), '2 hours 2 minutes 2 seconds')
+        self.assertEquals(format_lag(7322), '2 hours 2 minutes 2 seconds')
