8968a0b5c05d25e1b1c60f31ad00f1abc868a147
Fede Alvarez <federico.alvarez@ordergroove.com>
2018-11-12 16:15:10 -0300

    Resolve tests warnings

--- tests/nagios_plugin/test_lag_formatter.py
+++ tests/nagios_plugin/test_lag_formatter.py
@@ -8,12 +8,12 @@ from nagios_plugin.lag_formatter import format_lag
 class TestLagFormatter(unittest.TestCase):
 
     def test_response_contains_all_parts(self):
-        self.assertEquals(format_lag(3661), '1 hour 1 minute 1 second')
+        self.assertEqual(format_lag(3661), '1 hour 1 minute 1 second')
 
     def test_response_ignores_empty_parts(self):
-        self.assertEquals(format_lag(3601), '1 hour 1 second')
-        self.assertEquals(format_lag(61), '1 minute 1 second')
-        self.assertEquals(format_lag(3600), '1 hour')
+        self.assertEqual(format_lag(3601), '1 hour 1 second')
+        self.assertEqual(format_lag(61), '1 minute 1 second')
+        self.assertEqual(format_lag(3600), '1 hour')
 
     def test_response_adds_plural_to_formatted_parts(self):
-        self.assertEquals(format_lag(7322), '2 hours 2 minutes 2 seconds')
+        self.assertEqual(format_lag(7322), '2 hours 2 minutes 2 seconds')
